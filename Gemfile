2b99b2e3a9e29c2ddd0d1cf5d366dedb4da9ff41
Oldtimer <tiples@gmail.com>
2011-04-06 00:57:31 -0700

    una pa lang

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.5'
+gem 'will_paginate', '3.0.pre2'
+gem 'mysql2'
+
+group :development do
+	gem 'rspec-rails', '2.5.0'
+	gem 'annotate-models', '1.0.4'
+	gem 'faker', '0.3.1'
+end
+
+group :test do
+	gem 'rspec', '2.5.0'
+	gem 'webrat', '0.7.1'
+    gem 'factory_girl_rails', '1.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qms::Application.config.secret_token = 'df0bac410e6c1c9edac1c962ebb0a9ccb09a25774a1042e23747f90b1a81f46261b94467379650b3da3bfd3924450b8713eb600e54b93ae2893d9f36013bf0b0'
